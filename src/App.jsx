57a87457209073ad74380aeb132122cae9dd2c52
Diyor <baxacap@gmail.com>
2024-11-19 20:58:09 +0500

    calculator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Calculator from "./components/Calculator";
+
+const App = () => {
+  return (
+    <div>
+      <h1 style={{ textAlign: "center", marginTop: "20px" }}>
+        React Calculator
+      </h1>
+      <Calculator />
+    </div>
+  );
+};
+
+export default App;
